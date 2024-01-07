06730988e2420d9839b712feec04c6271d8959fb
Stern_Ritter <fadeef.k@yandex.ru>
2024-01-07 21:38:40 +0300

    Homework 7 patterns

--- otus-professional/hw07-patterns/src/main/java/ru/otus/HomeWork.java
+++ otus-professional/hw07-patterns/src/main/java/ru/otus/HomeWork.java
@@ -1,5 +1,22 @@
 package ru.otus;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.handler.ComplexProcessor;
+import ru.otus.handler.Handler;
+import ru.otus.listener.homework.HistoryListener;
+import ru.otus.model.Message;
+import ru.otus.model.ObjectForMessage;
+import ru.otus.processor.Processor;
+import ru.otus.processor.homework.BaseCurrentTimeProvider;
+import ru.otus.processor.homework.ProcessorSwapField11Field12;
+import ru.otus.processor.homework.ProcessorThrowsExceptionEvenSecond;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class HomeWork {
 
     /*
@@ -15,10 +32,49 @@ public class HomeWork {
          Для него уже есть тест, убедитесь, что тест проходит
     */
 
+    private static final Logger logger = LoggerFactory.getLogger(HomeWork.class);
+
     public static void main(String[] args) {
         /*
           по аналогии с Demo.class
           из элеменов "to do" создать new ComplexProcessor и обработать сообщение
         */
+
+        Processor processorSwapField11Field12 = new ProcessorSwapField11Field12();
+        Processor processorThrowsExceptionEvenSecond = new ProcessorThrowsExceptionEvenSecond(new BaseCurrentTimeProvider());
+        Consumer<Exception> errorHandler = (e) -> logger.info("Error: {}", e.getMessage());
+        List<Processor> processors = List.of(processorSwapField11Field12, processorThrowsExceptionEvenSecond);
+        Handler complexProcessor = new ComplexProcessor(processors, errorHandler);
+
+        HistoryListener historyListener = new HistoryListener();
+        complexProcessor.addListener(historyListener);
+
+        List<String> field13Data = new ArrayList<>();
+        field13Data.add("Field 13 value");
+        ObjectForMessage field13 = new ObjectForMessage();
+        field13.setData(field13Data);
+
+        long id = 1L;
+        Message message = new Message.Builder(id)
+                .field1("field1")
+                .field2("field2")
+                .field3("field3")
+                .field6("field6")
+                .field10("field10")
+                .field11("field11")
+                .field12("field12")
+                .field13(field13)
+                .build();
+
+        field13.setData(Collections.emptyList());
+        field13Data.clear();
+
+        Message resultMsg = complexProcessor.handle(message);
+        logger.info("Result message: {}", resultMsg);
+
+        Message historyMsg = historyListener.findMessageById(id).get();
+        logger.info("History message: {}", historyMsg);
+
+        complexProcessor.removeListener(historyListener);
     }
 }
--- otus-professional/hw07-patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ otus-professional/hw07-patterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -1,18 +1,28 @@
 package ru.otus.listener.homework;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+
 import ru.otus.listener.Listener;
 import ru.otus.model.Message;
 
 public class HistoryListener implements Listener, HistoryReader {
+    private final Map<Long, Message> messages;
+
+    public HistoryListener() {
+        this.messages = new HashMap<>();
+    }
 
     @Override
     public void onUpdated(Message msg) {
-        throw new UnsupportedOperationException();
+        Long id = msg.getId();
+        Message msgCopy = msg.toBuilder().build();
+        messages.put(id, msgCopy);
     }
 
     @Override
     public Optional<Message> findMessageById(long id) {
-        throw new UnsupportedOperationException();
+        return Optional.ofNullable(messages.get(id));
     }
 }
