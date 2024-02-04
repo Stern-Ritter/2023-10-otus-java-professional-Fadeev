159bb950bef979135e770c75f63b30216a0135f6
Stern_Ritter <fadeef.k@yandex.ru>
2024-02-04 18:41:29 +0300

    Homework 8 serialization

--- otus-professional/hw08-serialization/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ otus-professional/hw08-serialization/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,13 +1,26 @@
 package ru.otus.dataprocessor;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 public class FileSerializer implements Serializer {
+    private final File file;
+    private final ObjectMapper mapper;
 
-    public FileSerializer(String fileName) {}
+    public FileSerializer(String fileName) {
+        file = new File(fileName);
+        mapper = JsonMapper.builder().build();
+    }
 
     @Override
     public void serialize(Map<String, Double> data) {
-        // формирует результирующий json и сохраняет его в файл
+        try {
+            mapper.writeValue(file, data);
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
     }
 }
--- otus-professional/hw08-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ otus-professional/hw08-serialization/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,16 +1,35 @@
 package ru.otus.dataprocessor;
 
-import java.util.Collections;
-import java.util.List;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import ru.otus.model.Measurement;
 
-public class ResourcesFileLoader implements Loader {
+import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
-    public ResourcesFileLoader(String fileName) {}
+public class ResourcesFileLoader implements Loader {
+    private final File file;
+    private final ObjectMapper mapper;
+
+
+    public ResourcesFileLoader(String fileName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        String path = URLDecoder.decode(classLoader.getResource(fileName).getPath(), StandardCharsets.UTF_8);
+        file = new File(path);
+        mapper = JsonMapper.builder().build();
+    }
 
     @Override
     public List<Measurement> load() {
-        // читает файл, парсит и возвращает результат
-        return Collections.emptyList();
+        try {
+            return mapper.readValue(file, new TypeReference<>() {
+            });
+        } catch (IOException e) {
+            throw new FileProcessException(e);
+        }
     }
 }
