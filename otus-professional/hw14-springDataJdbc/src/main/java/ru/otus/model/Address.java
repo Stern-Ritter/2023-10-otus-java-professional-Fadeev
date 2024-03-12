0f7448308b07b57772cec4defb884ddb7901738b
Stern_Ritter <fadeef.k@yandex.ru>
2024-03-12 12:15:40 +0300

    Edits after review

--- otus-professional/hw14-springDataJdbc/src/main/java/ru/otus/model/Address.java
+++ otus-professional/hw14-springDataJdbc/src/main/java/ru/otus/model/Address.java
@@ -1,6 +1,5 @@
 package ru.otus.model;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceCreator;
@@ -8,11 +7,9 @@ import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Getter
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table("address")
 public class Address {
     @Id
-    @EqualsAndHashCode.Include
     private final Long id;
 
     @Column("street")
